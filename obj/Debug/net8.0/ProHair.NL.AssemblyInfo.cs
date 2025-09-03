d3692c85438be1e7dd4be74620569ddbdc3c1c0c
erol8383ates <erolates33@hotmail.com>
2025-09-03 19:58:33 +0200

    ilk committ

--- obj/Debug/net8.0/ProHair.NL.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProHair.NL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProHair.NL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9457dec7133d95030019188cc2bb2deaf65e730")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f43f01989f176ccc1118561a58111964e846d6a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProHair.NL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProHair.NL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

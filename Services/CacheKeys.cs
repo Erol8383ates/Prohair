224345fc399d6f818d2a531862c5d3e007c935c2
erol Ibili <98597953+Erol8383ates@users.noreply.github.com>
2025-09-16 13:27:02 +0200

    Create CacheKeys.cs

---
+++ Services/CacheKeys.cs
@@ -0,0 +1,8 @@
+namespace ProHair.NL.Services
+{
+    public static class CacheKeys
+    {
+        // Değer önemsiz; sadece değiştirildiğinde cache’i bozmak için kullanılır
+        public const string Stamp = "availability:stamp";
+    }
+}
